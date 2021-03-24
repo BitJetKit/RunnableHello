524402274ffa634da1b43cf6cacbfd5fa7b11be0
Jonathan M. Kelly <bitjetkit@gmail.com>
2021-03-24 06:49:40 -0500

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package RunnableHello;
+
+// By implementing the Runnable Interface, thread creation is possible.
+public class Hello implements Runnable{
+    public void run(){
+        
+    }
+} 
+class Test{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# RunnableHello
+ This is an introductory Java multithreading app: it utilizes Runnable
